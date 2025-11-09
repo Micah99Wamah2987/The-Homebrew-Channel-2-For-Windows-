453c5d3c1f58ee34c0bfd3fc1e84ac2af4c64f6d
Micah99Wamah2987 <146990157+Micah99Wamah2987@users.noreply.github.com>
2025-11-09 18:54:42 +0000

    Refactor main.cpp to include input functions
    Added input functions and modified main logic.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include "Scripts\images.hpp"
 #include "Scripts\videos.hpp"
 #include "Scripts\sounds.hpp"
+#include <iostream>
+#include <string>
+#include <fstream>
+
+void printf(std::string str, bool endl) {
+    if (endl) {
+        std::cout << str << std::endl;
+    } else if (!endl) {
+        std::cout << str;
+    }
+}
+
+int in_int() {
+    int rslt = (int)nullptr;
+    std::cin >> rslt;
+    return rslt;
+}
+
+std::string in_str() {
+    std::string rslt = nullptr;
+    std::cin >> rslt;
+    return rslt;
+}
 
 int main() {
-    
+    printf("What is the name of your project: ", false);
+    std::string name = in_str();
+    std::string app_name = name + ".dol";
 }
